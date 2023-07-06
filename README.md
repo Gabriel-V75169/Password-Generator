d9d703253ce6109c496a4296688f89ac6691a201
Gabriel Valencia <jrjr75169@gmail.com>
2023-07-06 01:11:06 -0700

    finished product

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Pasword-Generator
+# Password-Generator
+
+## Description
+
+Given the objective I added on to the code given to me allowing for users to create a randomly generated password using prompts and confirms that pop up on the screen and using that information it creates a randomly generated password
+
+## Getting Started
+I used google in order to find out which windows prompts I wanted to use and opted for confirms and using that made for loops for when those confirms were true and then used a random number generator I found on the web, then added them all together and had it display.
+
+### Dependencies
+
+* MacOS
+* Windows 10
+
+### Installing
+
+[Repository](https://github.com/Gabriel-V75169/Password-Generator)
+
+### Executing program
+
+[Pages](https://gabriel-v75169.github.io/Password-Generator/)
+
+## Author(s)
+
+Gabriel Valencia
+
+## Version History
+
+* 0.1
+    * Initial Release
+
+
+## License
+
+None for this project
+![Final Product](/assets/img/Screenshot%20(9).png)
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var passLen
+
+var upperCaseStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const upperCase = upperCaseStr.split('');
+
+var lowerCaseStr = "abcdefghijklmnopqrstuvwxyz"
+const lowerCase = lowerCaseStr.split('');
+
+var specialStr = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+const special = specialStr.split('');
+
+var numbStr = "0123456789"
+const numb = numbStr.split('');
+
+
+const generatePass = [];
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -9,6 +25,64 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+// Write password to the #password input
+function generatePassword() {
+  passLen = prompt("What is the length of the password between 8 and 128", "i.e. 10");
+  
+  
+
+  while (isNaN(passLen) || passLen > 128 || passLen < 8 || passLen - Math.floor(passLen) !== 0){
+    passLen = prompt("Invalid try again")
+  } 
+
+
+  for (let i = 0; i < passLen; i++){
+    generatePass[i] = lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  }
+  
+
+  
+  if (confirm("Do I include Capital Letters") == true) {
+    upperPrompt = true;
+  }
+
+  if (upperPrompt == true) {
+    for (let i = 0; i < passLen; i+=2){
+      generatePass[i] = upperCase[Math.floor(Math.random() * upperCase.length)];
+    } 
+  }
+  
+  
+
+if (confirm("Do I include Numbers") == true) {
+  numbPrompt = true;
+}
+  
+    
+if (numbPrompt == true) {
+  for (let i = 0; i < passLen; i+=3){
+    generatePass[i] = numb[Math.floor(Math.random() * numb.length)];
+  }
+}
+
+
+ 
+if (confirm("Do I include special characters") == true) {
+  specialPrompt = true;
+}
+
+if (specialPrompt == true) {
+  for (let i = 0; i < passLen; i+=4){
+    generatePass[i] = special[Math.floor(Math.random() * special.length)];
+  }
+}
+    
+ return generatePass.join("");
+}
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
